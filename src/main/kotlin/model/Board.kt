5aa6e248415acf48cb31599d10f19dc983a9c9b3
Gabriel Jayme <gabrielj_89@hotmail.com>
2024-06-18 18:11:02 -0300

    Add neighborsAssociating, randomizeMines, achievedGoal, onEvent, and restart methods in the Board.kt

--- src/main/kotlin/model/Board.kt
+++ src/main/kotlin/model/Board.kt
@@ -1,5 +1,8 @@
 package org.example.model
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 enum class EventBoard { VICTORY, DEFEAT }
 
 class Board(val rowsQuantity: Int, val columnQuantity: Int, private val mineQuantity: Int) {
@@ -18,6 +21,7 @@ class Board(val rowsQuantity: Int, val columnQuantity: Int, private val mineQuan
             fields.add(ArrayList())
             for (column in 0 until columnQuantity) {
                 val newField = Field(row, column)
+                newField.onEvent(this::checkingDefeatOrVictory)
                 fields[row].add(newField)
             }
         }
@@ -28,14 +32,61 @@ class Board(val rowsQuantity: Int, val columnQuantity: Int, private val mineQuan
     }
 
     private fun neighborsAssociating(field: Field) {
+        val (row, column) = field
+        val rows = arrayOf(row - 1, row, row + 1)
+        val columns = arrayOf(column - 1, column, column + 1)
 
+        rows.forEach { row ->
+            columns.forEach { column ->
+                val actual = fields.getOrNull(row)?.getOrNull(column)
+                actual?.takeIf { field != it }?.let { field.addNeighbors(it) }
+            }
+        }
     }
 
     private fun randomizeMines() {
+        val random = Random()
 
+        var sortedRow = -1
+        var sortedColumn = -1
+        var actualQuantityMines = 0
+
+        while (actualQuantityMines < this.mineQuantity) {
+            sortedRow = random.nextInt(rowsQuantity)
+            sortedColumn = random.nextInt(columnQuantity)
+
+            val sortedMine = fields[sortedRow][sortedColumn]
+            if (sortedMine.secure) {
+                sortedMine.mining()
+                actualQuantityMines++
+            }
+        }
+    }
+
+    private fun achievedGoal(): Boolean {
+        var winner = true
+        forEachField { if (!it.reachedGoal) winner = false }
+        return winner
+    }
+
+    private fun checkingDefeatOrVictory(field: Field, eventField: EventField) {
+        if (eventField == EventField.EXPLOSION) {
+            callbacks.forEach { it(EventBoard.DEFEAT) }
+        } else if (achievedGoal()) {
+            callbacks.forEach { it(EventBoard.VICTORY) }
+        }
     }
 
     fun forEachField(callback: (Field) -> Unit) {
         fields.forEach { row -> row.forEach(callback) }
+    }
+
+    fun onEvent(callback: (EventBoard) -> Unit) {
+        callbacks.add(callback)
+    }
+
+    fun restart() {
+        forEachField { it.restart() }
+        randomizeMines()
     }
 }
